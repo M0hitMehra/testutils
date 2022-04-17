f94f3ef8b460259d1fb1c7687ebb8840d99cbefa
M0hitMehra <786hitmen@gmail.com>
2022-04-17 21:27:19 +0530

    Updates

---
+++ precache-manifest.4ab16adc2775c879226839d2a929223a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe88831751070ea3ca60",
+    "url": "/testutils/static/css/main.73d20086.chunk.css"
+  },
+  {
+    "revision": "fe88831751070ea3ca60",
+    "url": "/testutils/static/js/main.fe888317.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/testutils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "284c82effe6d3f94e3ec",
+    "url": "/testutils/static/js/2.284c82ef.chunk.js"
+  },
+  {
+    "revision": "ad89bbd072e4505a9a44",
+    "url": "/testutils/static/js/runtime~main.ad89bbd0.js"
+  },
+  {
+    "revision": "a6b8421d864529898e515f72ad0b298f",
+    "url": "/testutils/index.html"
+  }
+];
